2514ff6f12276d398c53d505ffe1b979b81276b4
Israel Fariza <israelfn@hotmail.com>
2019-11-19 12:03:36 +0100

    Working on Cutadapt and Star alignment

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -1,39 +1,42 @@
 #Download all the files specified in data/urls
-for url in $(cat data/urls) #TODO
+
+for url in $(cat data/urls)
 do
     bash scripts/download.sh $url data
 done
 
 # Download the contaminants fasta file, and uncompress it
-bash scripts/download.sh https://bioinformatics.cnio.es/data/courses/decont/contaminants.fasta.gz res yes #TODO
+
+bash scripts/download.sh https://bioinformatics.cnio.es/data/courses/decont/contaminants.fasta.gz res yes
 
 # Index the contaminants file
+
 bash scripts/index.sh res/contaminants.fasta res/contaminants_idx
 
 # Merge the samples into a single file
-for sid in $(ls data/*.sRNA.fastq.gz | cut -d "_" -f1 | sed 's:data/::' | sort | uniq) #TODO
+
+for sid in $(ls data/*.sRNA.fastq.gz | cut -d "_" -f1 | sed 's:data/::' | sort | uniq)
 do
     bash scripts/merge_fastqs.sh data out/merged $sid
 done
 
-# TODO: run cutadapt for all merged files
+# Run cutadapt for all merged files
 
-echo "Running cutadapt..."
-mkdir -p log/cutadapt
-mkdir -p out/cutadapt
-
-#loop igual que el de abajo
-for
+for fname in out/merged/*.fastq.gz
 do
-cutadapt -m 18 -a TGGAATTCTCGGGTGCCAAGG --discard-untrimmed -o out/cutadapt/${sid}.trimmed.fastq.gz data/${sid}.fastq.gz > log/cutadapt/${sid}.log
+   sid=$(ls data xxxxxxxxxxxxxxxx fname XXXXXXXXXXXX)
 
-#loop igual ue  el de abajo  incluyedo cu
+    sid=$(ls out/merged/*.fastq.gz | cut -d "_" -f1 | sed 's:data/::' | sort | uniq)
+    mkdir -p log/cutadapt
+    mkdir -p out/cutadapt
+    cutadapt -m 18 -a TGGAATTCTCGGGTGCCAAGG --discard-untrimmed -o out/cutadapt/${sid}.trimmed.fastq.gz data/${sid}.fastq.gz > log/cutadapt/${sid}.log
+done
 
 #TODO: run STAR for all trimmed files
 for fname in out/trimmed/*.fastq.gz
 do
     # you will need to obtain the sample ID from the filename
-    sid=$1
+    sid=$(ls data/*.sRNA.fastq.gz | cut -d "_" -f1 | sed 's:data/::' | sort | uniq)
     mkdir -p out/star/$sid
     STAR --runThreadN 4 --genomeDir res/contaminants_idx --outReadsUnmapped Fastx --readFilesIn out/cutadapt/${sid} --readFilesCommand zcat --outFileNamePrefix out/star/${sid}
 done 
